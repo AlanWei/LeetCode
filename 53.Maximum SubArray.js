afb6c77e76b873703dece72c6183f057c0121cdb
Alan <alanweichangran@gmail.com>
2015-09-08 22:54:27 +1200

    include enumeration solution

--- 53.Maximum SubArray.js
+++ 53.Maximum SubArray.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 
+// quick
 var maxSubArray = function (nums) {
 	var max = nums[0];
 	var sum = 0;
@@ -23,3 +24,38 @@ var maxSubArray = function (nums) {
 	return max;
 }
 
+
+// enumeration
+var maxSubArray = function (nums) {
+    var max = nums[0];
+
+	for (var i = 1; i < nums.length + 1; i++) {
+
+		var sum = arraySum(nums, i);
+
+		if (sum > max) {
+			max = sum;
+		}
+	}
+
+	return max;
+};
+
+var arraySum = function (array, digits) {
+    var max = array[0];
+
+	for (var i = 0; i <= array.length - digits; i++) {
+
+		var sum = 0;
+
+		for (var j = 0; j < digits; j++) {
+			sum += array[i + j];
+		}
+
+		if (sum > max) {
+			max = sum;
+		}
+	}
+
+	return max;
+}
