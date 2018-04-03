5c1ee90609e3288a4242004c08172442018c4cb7
Alan Wei <alan.wei@shopback.com>
2018-04-03 11:55:33 +0800

    tweak 1&2

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -1,21 +1,25 @@
 /**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
+/**
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+/**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-
-function equal(first, second, target) {
-  return first + second === target;
-}
-
 var twoSum = function(nums, target) {
-  const length = nums.length;
-  for (let i=0; i<length; i++) {
-    const remain = length - i;
-    for (let j=1; j<remain; j++) {
-      if (equal(nums[i], nums[i+j], target)) {
-        return [i, i+j];
-      }
+  for (let i = 0; i < nums.length; i++) {
+    const matchIdx = nums.indexOf(target - nums[i]);
+    if (matchIdx !== -1 && matchIdx !== i) {
+      return [i, matchIdx];
     }
   }
 };
+
+console.log(twoSum([3, 2, 4], 6))
--- 2. Add Two Numbers.js
+++ 2. Add Two Numbers.js
@@ -29,17 +29,18 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+var addTwoNumbers = function(l1, l2) {
+  const ro = [];
 
-function add(l1, l2, carry, ro) {
-  if (l1 !== null || l2 !== null || carry !== 0) {
-    const val = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
-    ro.push(val % 10);
-    return add(l1 ? l1.next : null, l2 ? l2.next : null, Math.floor(val / 10), ro);
+  const add = (l1, l2, carry) => {
+    if (l1 !== null || l2 !== null || carry !== 0) {
+      const val = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+      ro.push(val % 10);
+      return add(l1 ? l1.next : null, l2 ? l2.next : null, Math.floor(val / 10));
+    }
   }
 
-  return ro;
-}
+  add(l1, l2, 0, []);
 
-var addTwoNumbers = function(l1, l2) {
-  return add(l1, l2, 0, []);
+  return ro;
 };
