249f88e296d8ca3a12d8b88651775b1abc7d1933
Alan Wei <alanweichangran@gmail.com>
2020-10-20 14:37:58 +0800

    add 300 dp solution

--- 300. Longest Increasing Subsequence.js
+++ 300. Longest Increasing Subsequence.js
@@ -5,11 +5,9 @@
 
 var lengthOfLIS = function (nums) {
   var len = nums.length;
-  var memo = new Array(len)
-    .fill(0)
-    .map(() => new Array(len).fill(-1));
+  var memo = new Array(len).fill(0).map(() => new Array(len).fill(-1));
 
-  var helper = function(nums, prev, cur) {
+  var helper = function (nums, prev, cur) {
     if (cur === len) {
       return 0;
     }
@@ -27,10 +25,34 @@ var lengthOfLIS = function (nums) {
     memo[prev + 1][cur] = Math.max(taken, notTaken);
 
     return memo[prev + 1][cur];
-  }
+  };
 
   return helper(nums, -1, 0);
 };
 
+// dp solution
+var lengthOfLIS = function (nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  var dp = new Array(nums.length).fill(1);
+  var maxAns = 1;
+
+  for (let i = 0; i < dp.length; i++) {
+    var longest = 0;
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        longest = Math.max(longest, dp[j]);
+      }
+      dp[i] = longest + 1;
+      maxAns = Math.max(maxAns, dp[i]);
+    }
+  }
+
+  return maxAns;
+};
+
 console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
 console.log(lengthOfLIS([10, 9, 2, 5, 3, 4]));
+ 
